915e8cbfea0813a2ff097332e9c4752fd79cc6c1
Bence Kovács <mail@bencekovacs.com>
2020-11-26 15:46:33 +0100

    add parameters

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -1,7 +1,7 @@
 /*
   ==============================================================================
 
-    This file contains the basic framework code for a JUCE plugin processor.
+	This file contains the basic framework code for a JUCE plugin processor.
 
   ==============================================================================
 */
@@ -12,149 +12,167 @@
 //==============================================================================
 SynthFaustPolyAudioProcessor::SynthFaustPolyAudioProcessor()
 #ifndef JucePlugin_PreferredChannelConfigurations
-     : AudioProcessor (BusesProperties()
-                     #if ! JucePlugin_IsMidiEffect
-                      #if ! JucePlugin_IsSynth
-                       .withInput  ("Input",  juce::AudioChannelSet::stereo(), true)
-                      #endif
-                       .withOutput ("Output", juce::AudioChannelSet::stereo(), true)
-                     #endif
-     )
+	: AudioProcessor(BusesProperties()
+#if ! JucePlugin_IsMidiEffect
+#if ! JucePlugin_IsSynth
+		.withInput("Input", juce::AudioChannelSet::stereo(), true)
+#endif
+		.withOutput("Output", juce::AudioChannelSet::stereo(), true)
+#endif
+	)
 #endif
 {
-    dspFaust.start();
+	for (int i = 1; i < 5; ++i)
+	{
+		String name = "gate";
+		name += i;
+		auto gate = new AudioParameterBool(name, name, false);
+		gate->addListener(this);
+		addParameter(gate);
+	}
+
+	auto cutoff = new AudioParameterFloat("Cutoff", "Cutoff", 50.0f, 10000.0f, 5000.0f);
+	cutoff->addListener(this);
+	addParameter(cutoff);
+
+	auto gain = new AudioParameterFloat("Gain", "Gain", 0.0f, 1.0f, 0.5f);
+	gain->addListener(this);
+	addParameter(gain);
+
+
+	dspFaust.start();
 }
 
 SynthFaustPolyAudioProcessor::~SynthFaustPolyAudioProcessor()
 {
-    dspFaust.stop();
+	dspFaust.stop();
 }
 
 void SynthFaustPolyAudioProcessor::keyOn(int pitch, int velocity)
 {
-    dspFaust.keyOn(pitch, velocity);
+	dspFaust.keyOn(pitch, velocity);
 
 }
 
 void SynthFaustPolyAudioProcessor::keyOff(int pitch)
 {
-    dspFaust.keyOff(pitch);
+	dspFaust.keyOff(pitch);
 }
 
 void SynthFaustPolyAudioProcessor::setCutoff(float cutoff)
 {
-    dspFaust.setParamValue("/Polyphonic/Voices/synth/cutoff", cutoff);
+	dspFaust.setParamValue("/Polyphonic/Voices/synth/cutoff", cutoff);
 }
 
 void SynthFaustPolyAudioProcessor::setGain(float gainOut)
 {
-    dspFaust.setParamValue("/Polyphonic/Voices/synth/gainOut", gainOut);
+	dspFaust.setParamValue("/Polyphonic/Voices/synth/gainOut", gainOut);
 }
 
 void SynthFaustPolyAudioProcessor::setGate1(bool gate1)
 {
-    if (gate1) {
-        dspFaust.setParamValue("/Polyphonic/Voices/synth/gate1", 1);
-    }
-    else {
-        dspFaust.setParamValue("/Polyphonic/Voices/synth/gate1", 0);
-    }
+	if (gate1) {
+		dspFaust.setParamValue("/Polyphonic/Voices/synth/gate1", 1);
+	}
+	else {
+		dspFaust.setParamValue("/Polyphonic/Voices/synth/gate1", 0);
+	}
 }
 
 void SynthFaustPolyAudioProcessor::setGate2(bool gate2)
 {
-    if (gate2) {
-        dspFaust.setParamValue("/Polyphonic/Voices/synth/gate2", 1);
-    }
-    else {
-        dspFaust.setParamValue("/Polyphonic/Voices/synth/gate2", 0);
-    }
+	if (gate2) {
+		dspFaust.setParamValue("/Polyphonic/Voices/synth/gate2", 1);
+	}
+	else {
+		dspFaust.setParamValue("/Polyphonic/Voices/synth/gate2", 0);
+	}
 }
 
 void SynthFaustPolyAudioProcessor::setGate3(bool gate3)
 {
-    if (gate3) {
-        dspFaust.setParamValue("/Polyphonic/Voices/synth/gate3", 1);
-    }
-    else {
-        dspFaust.setParamValue("/Polyphonic/Voices/synth/gate3", 0);
-    }
+	if (gate3) {
+		dspFaust.setParamValue("/Polyphonic/Voices/synth/gate3", 1);
+	}
+	else {
+		dspFaust.setParamValue("/Polyphonic/Voices/synth/gate3", 0);
+	}
 }
 
 void SynthFaustPolyAudioProcessor::setGate4(bool gate4)
 {
-    if (gate4) {
-        dspFaust.setParamValue("/Polyphonic/Voices/synth/gate4", 1);
-    }
-    else {
-        dspFaust.setParamValue("/Polyphonic/Voices/synth/gate4", 0);
-    }
+	if (gate4) {
+		dspFaust.setParamValue("/Polyphonic/Voices/synth/gate4", 1);
+	}
+	else {
+		dspFaust.setParamValue("/Polyphonic/Voices/synth/gate4", 0);
+	}
 }
 
 //==============================================================================
 const juce::String SynthFaustPolyAudioProcessor::getName() const
 {
-    return JucePlugin_Name;
+	return JucePlugin_Name;
 }
 
 bool SynthFaustPolyAudioProcessor::acceptsMidi() const
 {
-   #if JucePlugin_WantsMidiInput
-    return true;
-   #else
-    return false;
-   #endif
+#if JucePlugin_WantsMidiInput
+	return true;
+#else
+	return false;
+#endif
 }
 
 bool SynthFaustPolyAudioProcessor::producesMidi() const
 {
-   #if JucePlugin_ProducesMidiOutput
-    return true;
-   #else
-    return false;
-   #endif
+#if JucePlugin_ProducesMidiOutput
+	return true;
+#else
+	return false;
+#endif
 }
 
 bool SynthFaustPolyAudioProcessor::isMidiEffect() const
 {
-   #if JucePlugin_IsMidiEffect
-    return true;
-   #else
-    return false;
-   #endif
+#if JucePlugin_IsMidiEffect
+	return true;
+#else
+	return false;
+#endif
 }
 
 double SynthFaustPolyAudioProcessor::getTailLengthSeconds() const
 {
-    return 0.0;
+	return 0.0;
 }
 
 int SynthFaustPolyAudioProcessor::getNumPrograms()
 {
-    return 1;   // NB: some hosts don't cope very well if you tell them there are 0 programs,
-                // so this should be at least 1, even if you're not really implementing programs.
+	return 1;   // NB: some hosts don't cope very well if you tell them there are 0 programs,
+				// so this should be at least 1, even if you're not really implementing programs.
 }
 
 int SynthFaustPolyAudioProcessor::getCurrentProgram()
 {
-    return 0;
+	return 0;
 }
 
-void SynthFaustPolyAudioProcessor::setCurrentProgram (int index)
+void SynthFaustPolyAudioProcessor::setCurrentProgram(int index)
 {
 }
 
-const juce::String SynthFaustPolyAudioProcessor::getProgramName (int index)
+const juce::String SynthFaustPolyAudioProcessor::getProgramName(int index)
 {
-    return {};
+	return {};
 }
 
-void SynthFaustPolyAudioProcessor::changeProgramName (int index, const juce::String& newName)
+void SynthFaustPolyAudioProcessor::changeProgramName(int index, const juce::String& newName)
 {
 }
 
 //==============================================================================
-void SynthFaustPolyAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
+void SynthFaustPolyAudioProcessor::prepareToPlay(double sampleRate, int samplesPerBlock)
 {
 
 }
@@ -165,66 +183,104 @@ void SynthFaustPolyAudioProcessor::releaseResources()
 }
 
 #ifndef JucePlugin_PreferredChannelConfigurations
-bool SynthFaustPolyAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
+bool SynthFaustPolyAudioProcessor::isBusesLayoutSupported(const BusesLayout& layouts) const
 {
-  #if JucePlugin_IsMidiEffect
-    juce::ignoreUnused (layouts);
-    return true;
-  #else
-    // This is the place where you check if the layout is supported.
-    // In this template code we only support mono or stereo.
-    if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono()
-     && layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
-        return false;
+#if JucePlugin_IsMidiEffect
+	juce::ignoreUnused(layouts);
+	return true;
+#else
+	// This is the place where you check if the layout is supported.
+	// In this template code we only support mono or stereo.
+	if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono()
+		&& layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
+		return false;
 
-    // This checks if the input layout matches the output layout
-   #if ! JucePlugin_IsSynth
-    if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
-        return false;
-   #endif
+	// This checks if the input layout matches the output layout
+#if ! JucePlugin_IsSynth
+	if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
+		return false;
+#endif
 
-    return true;
-  #endif
+	return true;
+#endif
 }
 #endif
 
-void SynthFaustPolyAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juce::MidiBuffer& midiMessages)
+void SynthFaustPolyAudioProcessor::processBlock(juce::AudioBuffer<float>& buffer, juce::MidiBuffer& midiMessages)
 {
 
-    for (auto messageReference : midiMessages)
-    {
-        auto message = messageReference.getMessage();
-        if (message.isNoteOn())
-        {
-            keyOn(message.getNoteNumber(), message.getVelocity());
-        }
+	for (auto messageReference : midiMessages)
+	{
+		auto message = messageReference.getMessage();
+		if (message.isNoteOn())
+		{
+			keyOn(message.getNoteNumber(), message.getVelocity());
+		}
 
-        else if (message.isNoteOff())
-        {
-            keyOff(message.getNoteNumber());
-        }
+		else if (message.isNoteOff())
+		{
+			keyOff(message.getNoteNumber());
+		}
 
-        
-    }
+
+	}
 }
 
 //==============================================================================
 bool SynthFaustPolyAudioProcessor::hasEditor() const
 {
-    return true; // (change this to false if you choose to not supply an editor)
+	return true; // (change this to false if you choose to not supply an editor)
 }
 
 juce::AudioProcessorEditor* SynthFaustPolyAudioProcessor::createEditor()
 {
-    return new SynthFaustPolyAudioProcessorEditor (*this);
+	return new SynthFaustPolyAudioProcessorEditor(*this);
 }
 
 //==============================================================================
-void SynthFaustPolyAudioProcessor::getStateInformation (juce::MemoryBlock& destData)
+void SynthFaustPolyAudioProcessor::getStateInformation(juce::MemoryBlock& destData)
 {
 }
 
-void SynthFaustPolyAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
+void SynthFaustPolyAudioProcessor::setStateInformation(const void* data, int sizeInBytes)
+{
+}
+
+/// <summary>
+/// Call all the faust setter methods here.
+/// </summary>
+/// <param name="parameterIndex">The index of the parameter.</param>
+/// <param name="newValue">The normalised value of the parameter.</param>
+void SynthFaustPolyAudioProcessor::parameterValueChanged(int parameterIndex, float newValue)
+{
+	switch (parameterIndex)
+	{
+	case 0:
+		setGate1(newValue);
+		break;
+	case 1:
+		setGate2(newValue);
+		break;
+	case 2:
+		setGate3(newValue);
+		break;
+	case 3:
+		setGate4(newValue);
+		break;
+	case 4:
+		// cutoff
+		setCutoff(getParameters()[parameterIndex]->getCurrentValueAsText().getFloatValue());
+		break;
+	case 5:
+		// gain
+		setGain(getParameters()[parameterIndex]->getCurrentValueAsText().getFloatValue());
+		break;
+	default:
+		break;
+	}
+}
+
+void SynthFaustPolyAudioProcessor::parameterGestureChanged(int parameterIndex, bool gestureIsStarting)
 {
 }
 
@@ -232,5 +288,5 @@ void SynthFaustPolyAudioProcessor::setStateInformation (const void* data, int si
 // This creates new instances of the plugin..
 juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
 {
-    return new SynthFaustPolyAudioProcessor();
+	return new SynthFaustPolyAudioProcessor();
 }
--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -1,7 +1,7 @@
 /*
   ==============================================================================
 
-    This file contains the basic framework code for a JUCE plugin processor.
+	This file contains the basic framework code for a JUCE plugin processor.
 
   ==============================================================================
 */
@@ -14,59 +14,63 @@
 //==============================================================================
 /**
 */
-class SynthFaustPolyAudioProcessor  : public juce::AudioProcessor
+class SynthFaustPolyAudioProcessor : public juce::AudioProcessor,
+	AudioParameterBool::Listener
 {
 public:
-    void keyOn(int pitch, int  velocity);
-    void keyOff(int pitch);
-    void setCutoff(float cutoff);
-    void setGain(float gainOut);
-    void setGate1(bool gate1); // control the square wave
-    void setGate2(bool gate2); //control the saw wave
-    void setGate3(bool gate3); //control the triangle wave
-    void setGate4(bool gate4); //control the noise wave
+	void keyOn(int pitch, int  velocity);
+	void keyOff(int pitch);
+	void setCutoff(float cutoff);
+	void setGain(float gainOut);
+	void setGate1(bool gate1); // control the square wave
+	void setGate2(bool gate2); //control the saw wave
+	void setGate3(bool gate3); //control the triangle wave
+	void setGate4(bool gate4); //control the noise wave
 
-    //==============================================================================
-    SynthFaustPolyAudioProcessor();
-    ~SynthFaustPolyAudioProcessor() override;
+	//==============================================================================
+	SynthFaustPolyAudioProcessor();
+	~SynthFaustPolyAudioProcessor() override;
 
-    //==============================================================================
-    void prepareToPlay (double sampleRate, int samplesPerBlock) override;
-    void releaseResources() override;
+	//==============================================================================
+	void prepareToPlay(double sampleRate, int samplesPerBlock) override;
+	void releaseResources() override;
 
-   #ifndef JucePlugin_PreferredChannelConfigurations
-    bool isBusesLayoutSupported (const BusesLayout& layouts) const override;
-   #endif
+#ifndef JucePlugin_PreferredChannelConfigurations
+	bool isBusesLayoutSupported(const BusesLayout& layouts) const override;
+#endif
 
-    void processBlock (juce::AudioBuffer<float>&, juce::MidiBuffer&) override;
+	void processBlock(juce::AudioBuffer<float>&, juce::MidiBuffer&) override;
 
-    //==============================================================================
-    juce::AudioProcessorEditor* createEditor() override;
-    bool hasEditor() const override;
+	//==============================================================================
+	juce::AudioProcessorEditor* createEditor() override;
+	bool hasEditor() const override;
 
-    //==============================================================================
-    const juce::String getName() const override;
+	//==============================================================================
+	const juce::String getName() const override;
 
-    bool acceptsMidi() const override;
-    bool producesMidi() const override;
-    bool isMidiEffect() const override;
-    double getTailLengthSeconds() const override;
+	bool acceptsMidi() const override;
+	bool producesMidi() const override;
+	bool isMidiEffect() const override;
+	double getTailLengthSeconds() const override;
 
-    //==============================================================================
-    int getNumPrograms() override;
-    int getCurrentProgram() override;
-    void setCurrentProgram (int index) override;
-    const juce::String getProgramName (int index) override;
-    void changeProgramName (int index, const juce::String& newName) override;
+	//==============================================================================
+	int getNumPrograms() override;
+	int getCurrentProgram() override;
+	void setCurrentProgram(int index) override;
+	const juce::String getProgramName(int index) override;
+	void changeProgramName(int index, const juce::String& newName) override;
 
-    //==============================================================================
-    void getStateInformation (juce::MemoryBlock& destData) override;
-    void setStateInformation (const void* data, int sizeInBytes) override;
+	//==============================================================================
+	void getStateInformation(juce::MemoryBlock& destData) override;
+	void setStateInformation(const void* data, int sizeInBytes) override;
 
 private:
-	
-    DspFaust dspFaust;
-	
-    //==============================================================================
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SynthFaustPolyAudioProcessor)
+
+	DspFaust dspFaust;
+
+	//==============================================================================
+	JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(SynthFaustPolyAudioProcessor)
+
+	void parameterValueChanged(int parameterIndex, float newValue) override;
+	void parameterGestureChanged(int parameterIndex, bool gestureIsStarting) override;
 };
